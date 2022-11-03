d55fb7623a99db3ea03257d8a4e9bc17084b3031
Silva Marcus <92951909+SilvaMarcus@users.noreply.github.com>
2022-11-02 23:56:44 -0300

    feat: Requisicao assincrona

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,9 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 
-fetch(url,).then((response) => {
-  console.log(response);
-})
-     
+fetch(url,)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.log(error))
 
-const x = 10 + 10 
 
-console.log(x);
